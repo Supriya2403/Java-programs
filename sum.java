1e66859a1e3077376601f772caf69c5424a9ad76
Supriya2403 <supriyasonij6@gmail.com>
2025-04-10 11:22:24 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,24 @@
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+import java.util.Scanner;
+
+public class sumoftwo {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a=sc.nextInt();
+        System.out.println("Enter second number:");
+        int b=sc.nextInt();
+        int sum=a+b;
+    
+        System.out.println("The sum is:" +sum);
+    }
+    }
+
+
+
+
+
