53ba70cb9e33946cde532386706f78c19a7c8f63
Supriya2403 <supriyasonij6@gmail.com>
2025-04-10 11:10:29 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,18 @@
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+import java.util.Scanner;
+
+public class bitwise {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        System.out.println("a ^ b = " + (a ^ b));
+    }
+}
+
+
+
+
