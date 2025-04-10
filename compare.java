e9ee938a217b6c5bae0a386561dc5938523c8496
Supriya2403 <supriyasonij6@gmail.com>
2025-04-10 10:58:57 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,46 @@
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+import java.util.Scanner;
+
+public class Compare{
+    public static void main(String[] args) {
+        int num1=2;
+        int num2=4;
+if(num1>num2){
+    System.Out.println("num1 is greater than num2");
+    
+}
+elseif{
+    System.out.println("num2 is greater than num1");
+}
+elseif{
+   System.out.println("num1 is equal to the num2"); 
+}
+}
+    
+        int a = sc.nextInt();
+
+        System.out.println("Enter the second number:");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("The sum is: " + sum);
+
+        int sub = a - b;
+        System.out.println("The subtraction is: " + sub);
+
+        int multiply = a * b;
+        System.out.println("The multiplication is: " + multiply);
+
+        
+            int div = a / b;
+            System.out.println("The division is: " + div);
+        
+
+        sc.close();
+    }
+}
