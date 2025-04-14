5dcba8a4190611d125fc552acf69cf78ab59d836
Supriya2403 <supriyasonij6@gmail.com>
2025-04-14 09:51:52 +0530

    Add files via upload

---
+++ incrementdecrement.py
@@ -0,0 +1,16 @@
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main{
+    public static void main(String[] args) {
+        int x=4;
+        
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(--x);
+    }
+}
