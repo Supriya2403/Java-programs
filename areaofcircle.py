ffc434c7f2458f9e966a960a11ca6244e68e9ea3
Supriya2403 <supriyasonij6@gmail.com>
2025-04-14 11:09:01 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,16 @@
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int radius=12;
+        double pi=3.1415;
+        double area= pi*radius*radius;
+        System.out.println("The area of circle:"+area);
+        
+    }
+}
