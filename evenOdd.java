47a6f1dab3d4b9779af5fc60484a8b6e2cbde5cc
Supriya2403 <supriyasonij6@gmail.com>
2025-05-12 15:06:11 +0530

    Add files via upload

---
+++ evenOdd.java
@@ -0,0 +1,24 @@
+mport java.util.Scanner;
+
+
+class evenOdd{
+
+	
+	public static void main(String[] args)
+	{
+	
+		int num = 10;
+	num % 2 == 0) {
+
+
+			System.out.println("Entered Number is Even");
+		}
+
+		else {
+
+System.out.println("Entered Number is Odd");
+		}
+	}
+}
+
+		
